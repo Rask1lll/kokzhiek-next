1b7837ac5791dd6abcf4b05d2edd704107eab347
sateras <satpekov02@mail.ru>
2025-12-08 11:38:54 +0500

    style: btn color

--- app/components/bookDetailsPage.tsx/CreateChapterButton.tsx
+++ app/components/bookDetailsPage.tsx/CreateChapterButton.tsx
@@ -10,7 +10,7 @@ export default function CreateChapterButton() {
       onClick={() => {
         addContent(<CreateChapterModalWindow />);
       }}
-      className="w-full bg-blue-300/50 cursor-pointer items-center border-blue-400 hover:bg-blue-300 rounded-xl p-3 px-5 border-4 border-dashed flex transition-all duration-150 hover:border-blue-500 gap-5"
+      className="w-full bg-blue-300/50 cursor-pointer items-center border-blue-400 hover:bg-blue-300/70 rounded-xl p-3 px-5 border-4 border-dashed flex transition-all duration-150 hover:border-blue-500 gap-5"
     >
       <PiPlus className="w-7 h-7 text-3xl" /> <p>Добавить Главу</p>
     </button>
