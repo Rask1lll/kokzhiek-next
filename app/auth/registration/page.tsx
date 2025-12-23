b7bf6b1ee671ac387db35f8ac25a32ea12ed5491
sateras <satpekov02@mail.ru>
2025-12-24 02:19:42 +0500

    feat: key

--- app/auth/registration/page.tsx
+++ app/auth/registration/page.tsx
@@ -38,6 +38,7 @@ export default function RegisterPage() {
             email,
             password,
             password_confirmation: confirmPassword,
+            ...(registrationKey && { activation_key: registrationKey }),
           }),
         }
       );
