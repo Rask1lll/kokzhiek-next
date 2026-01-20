a08098a45e634e8e567661bbfd7b67f246623dab
sateras <satpekov02@mail.ru>
2026-01-20 13:58:03 +0500

    CACHE_NAME

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kokzhiek-v1.02";
+const CACHE_NAME = "kokzhiek-v1.03";
 const BOOKS_PAGE = "/books";
 const BOOK_PAGE = "/books/book";
 const CHAPTER_PAGE = "/books/book/chapter";
